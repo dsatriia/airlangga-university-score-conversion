347029b9ee935724157a951a1d494e5c56c6eed6
Dimas Satria <dimassatriia@gmail.com>
2020-10-06 21:07:10 +0700

    Full feature commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,140 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/unair.jpg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row class="text-center">
+      <v-col></v-col>
+      <v-col cols="6" class="mb-4">
+        <h1 class="display-1 font-weight-bold mb-3">
+          Airlangga University Score Conversion
+        </h1>
+
+        <v-form v-model="valid">    
+          <v-row>
+  
+              <v-text-field
+                v-model="scoreValue"
+                :rules="scoreRules"                
+                label="Your score"
+                required
+              ></v-text-field>               
+          </v-row>    
+        </v-form>
+        
+        <v-card
+            :loading="loading"
+            class="mx-auto mb-5"
+            max-width="374"
+          >
+            <template slot="progress">
+              <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+
+
+            <v-card-title>Conversion result :</v-card-title>
+
+            <v-card-text class="text-center">
+              <v-chip-group
+                v-model="selection"
+                active-class="deep-purple accent-4 white--text"
+                column
+              >
+                   <v-avatar
+                    class="mx-auto"
+                    color="indigo"
+                    size="80"
+                  >
+                    <span class="white--text headline">{{convertScore()}}</span>
+                  </v-avatar>
+
+              </v-chip-group>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                color="deep-purple lighten-2"
+                text
+                @click="reserve"
+              >
+                Reset
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+        <p class="subheading font-weight-regular">
+          Build with
+          <v-icon class="mr-1"
+            medium
+            color="red darken-2"
+          >
+            mdi-cards-heart
+          </v-icon>
+          <a
+            href="https://github.com/dsatriia"
+            target="_blank"
+          >dsatriia</a> © 2020
+        </p>
+      </v-col>
+      <v-col></v-col>
+      
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+      loading: false,
+      selection: 1,
+      valid: false,
+      scoreValue: '',
+      scoreRules: [
+        v => !!v || 'Score is required',                        
+      ]
+    }),
+    methods: {
+      reserve () {
+        const vm =this;
+        this.loading = true
+
+        setTimeout(function(){vm.loading = false; vm.scoreValue = ''}, 1000);
+      },
+      convertScore(){
+        if(this.scoreValue == ''){
+          return '-'
+        } else if(parseFloat(this.scoreValue) >= 0 && parseFloat(this.scoreValue) < 40){
+          return 'E'
+        } else if(parseFloat(this.scoreValue) >= 40 && parseFloat(this.scoreValue) < 55){
+          return 'D'
+        } else if(parseFloat(this.scoreValue) >= 55 && parseFloat(this.scoreValue) < 60){
+          return 'C'
+        } else if(parseFloat(this.scoreValue) >= 60 && parseFloat(this.scoreValue) < 65){
+          return 'BC'
+        } else if(parseFloat(this.scoreValue) >= 65 && parseFloat(this.scoreValue) < 70){
+          return 'B'
+        } else if(parseFloat(this.scoreValue) >= 70 && parseFloat(this.scoreValue) < 75){
+          return 'AB'
+        } else if(parseFloat(this.scoreValue) >= 75 && parseFloat(this.scoreValue) <= 100){
+          return 'A'
+        } else {
+          return '-'
+        }
+      }
+    },
+  }
+</script>
